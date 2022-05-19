9faf5fa3a09961de7ffcacfb70f030a8dcd19aa4
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-05-19 12:53:51 +0530

    ⌨️ add `npm install`

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@ cd $GITHUB_WORKSPACE
 git config user.name "Chiroyce1"
 git config user.email "Chiroyce1@users.noreply.github.com"
 
+npm install
 npm run build
-git checkout -b gh-pages
-git commit -am "Built"
-git checkout -b main
+git checkout gh-pages # hopefully the -b flag isn't needed for an already existent branch
+git checkout main
 rm -rf public/build/
 
 git add . # since new files are generated we need to include them
 echo "[commit.sh]: committing locally"
-git commit -m "Build run" -a
+git commit -m "Build Run" -a
 echo "[commit.sh]: pushing to github"
 git push
 echo "completed commit.sh!!"
