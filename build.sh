be8fc7c06fde82d5ac85c6e2633d4f7e8bf25b18
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-05-19 12:55:38 +0530

    ⬆️ Fix push to GitHub

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ npm run build
 git checkout gh-pages # hopefully the -b flag isn't needed for an already existent branch
 git checkout main
 rm -rf public/build/
+git checkout gh-pages # go back to gh-pages, only then it pushes to github
 
 git add . # since new files are generated we need to include them
 echo "[commit.sh]: committing locally"
