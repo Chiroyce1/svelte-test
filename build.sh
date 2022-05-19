05e89f8a0f257078aea5e626b3a8c0a8cccb0161
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-05-19 12:58:21 +0530

    ✨ Use GitHub Actions to switch branch instead of git

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@ cd $GITHUB_WORKSPACE
 git config user.name "Chiroyce1"
 git config user.email "Chiroyce1@users.noreply.github.com"
 
-git checkout gh-pages # hopefully the -b flag isn't needed for an already existent branch
 npm install
 npm run build
-git checkout main
-rm -rf public/build/
-git checkout gh-pages # go back to gh-pages, only then it pushes to github
 
 git add . # since new files are generated we need to include them
 echo "[commit.sh]: committing locally"
